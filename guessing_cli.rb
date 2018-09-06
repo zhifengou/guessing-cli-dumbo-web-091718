8830f6a827bc93cf681efd88c0118cddd3d40742
zhifeng ou <zhifengou@hotmail.com>
2018-09-06 16:50:26 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,23 +16,4 @@ loop do
   end
 end
 end
-
-# def run_guessing_game
-#   loop do
-#       puts "Guess a number between 1 and 6."
-#       guess = gets.chomp
-#       computer_number = rand(1..6)
-#       if guess == "exit"
-#         puts "Goodbye!"
-#         break
-#       elsif guess.to_i == computer_number
-#           puts "You guessed the correct number!"
-#         elsif guess.to_i != computer_number
-#           puts "The computer guessed #{computer_number}."
-#         else
-#           puts "Invalid input"
-#         end
-#       end
-#     end
-    
   
